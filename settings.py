33a6f07aa512142d9ab09af026e1c90d7d53c5e5
ATHEXgod <moditrathor@gmail.com>
2024-08-10 10:52:23 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import os
 
-API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN', 'your_default_bot_token')  # Set your Telegram Bot API token
+API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN', '7242067053:AAHLoLKbLZhAcPzmmEIPcDQCxmVtk1iKsKU')  # Set your Telegram Bot API token
 RAZORPAY_KEY_ID = os.getenv('RAZORPAY_KEY_ID', 'your_default_key_id')  # Set your Razorpay Key ID
 RAZORPAY_KEY_SECRET = os.getenv('RAZORPAY_KEY_SECRET', 'your_default_key_secret')  # Set your Razorpay Key Secret
-MONGO_DB_URI = os.getenv('MONGO_DB_URI', 'your_default_mongo_uri')  # Set your MongoDB URI
+MONGO_DB_URI = os.getenv('MONGO_DB_URI', 'mongodb+srv://moditrathor:4BG38960UgCWZXbS@cluster0.sdkc4bt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')  # Set your MongoDB URI
 PAYMENT_FEATURE_ENABLED = os.getenv('PAYMENT_FEATURE_ENABLED', 'False').lower() == 'true'  # Set to True or False
 # config/settings.py
 
